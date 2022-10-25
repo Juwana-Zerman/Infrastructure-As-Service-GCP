28fbc44b6de6bc6cc59adfbe274a8e06a9ec6edd
Juwana Zerman <juwanazerman@gmail.com>
2022-10-24 23:46:13 -0400

    Update main.tf vm name & machine type

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,18 @@
 provider "google" {
   # Path to GCP service account credentials
-  credentials = file("terraform/terraform.tfvars")
+  credentials = var.credentials
   # Your GCP project ID
   project = "infrastructure-as-code-366522"
   # Any region of your choice
   region = "us-east4"
   # Any zone of your choice
-  zone = "us-east4-a"
+  zone = "us-east4-b"
 }
 
 resource "google_compute_instance" "vm" {
-  name         = "IaaS-vm"
-  machine_type = "f1-micro"
-  zone         = "us-east4-a"
+  name         = "iaas-vm"
+  machine_type = "e2-micro"
+  zone         = "us-east4-b"
 
   boot_disk {
     initialize_params {
